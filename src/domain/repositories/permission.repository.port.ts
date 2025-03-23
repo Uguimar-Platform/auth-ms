7feb2910fba21572fb0e29a521368c58e5f99c50
Pedro Holguin <holguinpedro90@gmail.com>
2025-03-22 21:53:45 -0500

    :fire: Add login, roles, permissions Auth Microservices

--- src/domain/repositories/permission.repository.port.ts
+++ src/domain/repositories/permission.repository.port.ts
@@ -1,5 +1,9 @@
-import { PermissionsModel } from "../models/permissions.model";
+import { PermissionsModel } from '../models/permissions.model';
 
 export interface PermissionsRepositoryPort {
   createPermission(permisison: PermissionsModel);
+  findPermissionById(id: number);
+  updatePermission(id: number, module: PermissionsModel);
+  updatePermissionStatus(id: number, status: number);
+  listPermissionPaginated(filter: PermissionsModel);
 }
--- src/infrastructure/repositories/prisma.permission.repository.adapter.ts
+++ src/infrastructure/repositories/prisma.permission.repository.adapter.ts
@@ -1,17 +1,32 @@
-import { Injectable } from "@nestjs/common";
-import { PermissionsModel } from "src/domain/models/permissions.model";
-import { PermissionsRepositoryPort } from "src/domain/repositories/permission.repository.port";
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PermissionsModel } from 'src/domain/models/permissions.model';
+import { PermissionsRepositoryPort } from 'src/domain/repositories/permission.repository.port';
 
-@Injectable()    
-export class PrismaPermissionRepositoryAdapter implements PermissionsRepositoryPort {
-    
-    createPermission(permisison: PermissionsModel): Promise<PermissionsModel> {
-        throw new Error("Method not implemented.");
-    }
-   
-    
-   
+@Injectable()
+export class PrismaPermissionRepositoryAdapter
+  implements PermissionsRepositoryPort
+{
+  constructor(private prisma: PrismaClient) {}
 
+  async createPermission(permisision: PermissionsModel) {
+    const createPermission = await this.prisma.permission.create({
+      data: permisision,
+    });
 
+    return createPermission;
+  }
+
+  async findPermissionById(id: number) {
     
+  }
+  async updatePermission(id: number, module: PermissionsModel) {
+    throw new Error('Method not implemented.');
+  }
+  async updatePermissionStatus(id: number, status: number) {
+    throw new Error('Method not implemented.');
+  }
+  async listPermissionPaginated(filter: PermissionsModel) {
+    throw new Error('Method not implemented.');
+  }
 }
