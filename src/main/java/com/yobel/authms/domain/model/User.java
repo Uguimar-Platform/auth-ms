d72d422537ffa7ab5e0e6ed253699377b946db63
TadeoPG <tadeomanuelportillo@gmail.com>
2025-04-13 06:07:49 -0500

    feat: add builder.default when it is necessary

--- src/main/java/com/yobel/authms/domain/model/User.java
+++ src/main/java/com/yobel/authms/domain/model/User.java
@@ -17,6 +17,7 @@ public class User {
     private String username;
     private String email;
     private String password;
+    @Builder.Default
     private Set<Role> roles = new HashSet<>();
     private boolean enabled;
 
--- src/main/java/com/yobel/authms/infrastructure/input/rest/dto/LoginResponse.java
+++ src/main/java/com/yobel/authms/infrastructure/input/rest/dto/LoginResponse.java
@@ -15,5 +15,6 @@ public class LoginResponse {
     private String accessToken;
     private String refreshToken;
     private Instant expiryDate;
+    @Builder.Default
     private String tokenType = "Bearer";
 }
